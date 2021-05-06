8ea95f2aaf52f311aebf245f37b325abca04a6d2
rhish9h <rhish9h@gmail.com>
2021-05-06 16:48:35 +0530

    initial commit - Basic spring boot app

---
+++ stockyourfridge/src/main/java/com/stockyourfridge/stockyourfridge/StockyourfridgeApplication.java
@@ -0,0 +1,13 @@
+package com.stockyourfridge.stockyourfridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockyourfridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockyourfridgeApplication.class, args);
+	}
+
+}
---
+++ stockyourfridge/src/test/java/com/stockyourfridge/stockyourfridge/StockyourfridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockyourfridge.stockyourfridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockyourfridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
